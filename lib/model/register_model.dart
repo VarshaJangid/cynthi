173d4fe42710b4bbe31d602c7c2dcf21523bfeb4
Varsha Jangid <75119082+VarshaJangid@users.noreply.github.com>
2022-02-23 00:13:49 +0530

    create register model

---
+++ lib/model/register_model.dart
@@ -0,0 +1,33 @@
+// To parse this JSON data, do
+//
+//     final registerModel = registerModelFromJson(jsonString);
+
+import 'dart:convert';
+
+RegisterModel registerModelFromJson(String str) => RegisterModel.fromJson(json.decode(str));
+
+String registerModelToJson(RegisterModel data) => json.encode(data.toJson());
+
+class RegisterModel {
+  RegisterModel({
+    this.status = '',
+    this.message = '',
+    this.token = '',
+  });
+
+  String status;
+  String message;
+  String token;
+
+  factory RegisterModel.fromJson(Map<String, dynamic> json) => RegisterModel(
+    status: json["status"],
+    message: json["message"],
+    token: json["token"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "status": status,
+    "message": message,
+    "token": token,
+  };
+}
