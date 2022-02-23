32017d24ab886b1a6a9505bf4a2d8d95ab84ddbf
Varsh Jangid <varshajangid1999@gmail.com>
2022-02-23 12:05:36 +0530

    update otp verify screen

--- lib/ui/otp_verify/otp_verify_screen.dart
+++ lib/ui/otp_verify/otp_verify_screen.dart
@@ -1,12 +1,13 @@
-import 'package:cynthi/ui/component/custom_button.dart';
-import 'package:cynthi/ui/login/login_view_model.dart';
-import 'package:cynthi/utils/app_constant.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import '/ui/login/login_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
-import 'package:pin_code_fields/pin_code_fields.dart';
+import '/utils/app_constant.dart';
 
 class OtpVerifyScreen extends StatelessWidget {
-  const OtpVerifyScreen({Key? key}) : super(key: key);
+  const OtpVerifyScreen({Key? key, required this.mobileNumber}) : super(key: key);
+  final String mobileNumber;
+
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +18,9 @@ class OtpVerifyScreen extends StatelessWidget {
         return Scaffold(
           floatingActionButton: FloatingActionButton(
             elevation: 0,
-            onPressed: () {},
+            onPressed: () {
+              viewModel.verifyOTP(context, mobileNumber);
+              },
             backgroundColor: Colors.grey,
             child: const Icon(Icons.arrow_forward_ios),
           ),
@@ -31,68 +34,69 @@ class OtpVerifyScreen extends StatelessWidget {
                   Constants.verifyYourNumber,
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
                 ),
-                const SizedBox(height: 200),
-                // PinCodeTextField(
-                //   appContext: context,
-                //   pastedTextStyle: TextStyle(
-                //     color: Colors.green.shade600,
-                //     fontWeight: FontWeight.bold,
-                //   ),
-                //   length: 4,
-                //   obscureText: true,
-                //   obscuringCharacter: '*',
-                //   obscuringWidget: FlutterLogo(
-                //     size: 24,
-                //   ),
-                //   blinkWhenObscuring: true,
-                //   animationType: AnimationType.fade,
-                //   validator: (v) {
-                //     if (v!.length < 3) {
-                //       return "I'm from validator";
-                //     } else {
-                //       return null;
-                //     }
-                //   },
-                //   pinTheme: PinTheme(
-                //     shape: PinCodeFieldShape.box,
-                //     borderRadius: BorderRadius.circular(5),
-                //     fieldHeight: 50,
-                //     fieldWidth: 40,
-                //     activeFillColor: Colors.white,
-                //   ),
-                //   cursorColor: Colors.black,
-                //   animationDuration: Duration(milliseconds: 300),
-                //   enableActiveFill: true,
-                //   errorAnimationController: errorController,
-                //   controller: textEditingController,
-                //   keyboardType: TextInputType.number,
-                //   boxShadows: [
-                //     BoxShadow(
-                //       offset: Offset(0, 1),
-                //       color: Colors.black12,
-                //       blurRadius: 10,
-                //     )
-                //   ],
-                //   onCompleted: (v) {
-                //     print("Completed");
-                //   },
-                //   // onTap: () {
-                //   //   print("Pressed");
-                //   // },
-                //   onChanged: (value) {
-                //     print(value);
-                //     // setState(() {
-                //     //   currentText = value;
-                //     // });
-                //   },
-                //   beforeTextPaste: (text) {
-                //     print("Allowing to paste $text");
-                //     //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
-                //     //but you can show anything you want here, like your pop up saying wrong paste format or etc
-                //     return true;
-                //   },
-                // ),
-                TextField
+                const SizedBox(height: 100),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 110),
+                  child: PinCodeTextField(
+                    appContext: context,
+                    pastedTextStyle: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    length: 4,
+                    obscureText: true,
+                    obscuringCharacter: '*',
+                    blinkWhenObscuring: true,
+                    animationType: AnimationType.fade,
+                    validator: (v) {
+                      if (v!.length < 3) {
+                        return "I'm from validator";
+                      } else {
+                        return null;
+                      }
+                    },
+                    pinTheme: PinTheme(
+                      shape: PinCodeFieldShape.box,
+                      borderRadius: BorderRadius.circular(5),
+                      fieldHeight: 45,
+                      fieldWidth: 40,
+                      activeFillColor: Colors.white,
+                      activeColor: Colors.white,
+                      selectedColor: Colors.white,
+                      inactiveColor: Colors.white,
+                      inactiveFillColor: Colors.white,
+                      selectedFillColor: Colors.white,
+                    ),
+                    controller: viewModel.otpController,
+                    cursorColor: Colors.black,
+                    animationDuration: const Duration(milliseconds: 300),
+                    enableActiveFill: true,
+                    keyboardType: TextInputType.number,
+                    boxShadows: const [
+                      BoxShadow(
+                        offset: Offset(0, 1),
+                        color: Colors.black12,
+                        blurRadius: 10,
+                      )
+                    ],
+                    onCompleted: (v) {
+                      print("Completed");
+                      print(v);
+                      // viewModel.verifyOTP(context);
+                    },
+                    onChanged: (value) {
+                      print(value);
+                      // setState(() {
+                      //   currentText = value;
+                      // });
+                    },
+                    beforeTextPaste: (text) {
+                      print("Allowing to paste $text");
+                      return true;
+                    },
+                  ),
+                ),
+                const SizedBox(height: 100),
                 InkWell(
                   onTap: () {},
                   child: Padding(
